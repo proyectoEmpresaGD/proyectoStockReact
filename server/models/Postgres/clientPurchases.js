f172c6684ebfd9fa0aa5a0dd32a2cae11dfcc253
DavidSM14 <esilian14@gmail.com>
2026-05-26 07:30:33 +0200

    Version 3.5.5

--- server/models/Postgres/clientPurchases.js
+++ server/models/Postgres/clientPurchases.js
@@ -15,36 +15,32 @@ export class ClientPurchasesModel {
                 avl.dt2,
                 avl.dt3,
 
-                fv.codserfacventa,
-                fv.nfacventa,
-                fv.fecha,
-                fv.ejercicio,
+                av.codserfacventa,
+                av.nfacventa,
+                av.fecha,
+                av.ejercicio,
 
                 av.codseralbventa,
                 av.nalbventa,
 
                 avl.linea
-            FROM facventa fv
-            INNER JOIN albventa av
-                ON av.codserfacventa = fv.codserfacventa
-                AND av.nfacventa = fv.nfacventa
-                AND av.codclien = fv.codclien
+            FROM albventa av
             INNER JOIN albventa_linea avl
                 ON avl.codseralbventa = av.codseralbventa
                 AND avl.nalbventa = av.nalbventa
                 AND avl.codclien = av.codclien
-            WHERE fv.codclien = $1
+            WHERE av.codclien = $1
                 AND avl.codprodu IS NOT NULL
                 AND TRIM(avl.codprodu) <> ''
         `;
 
         if (ejercicio && ejercicio.trim() !== '') {
             params.push(Number(ejercicio));
-            query += ` AND fv.ejercicio = $${params.length}`;
+            query += ` AND av.ejercicio = $${params.length}`;
         }
 
         query += `
-            ORDER BY fv.fecha DESC, av.nalbventa DESC, avl.linea ASC
+            ORDER BY av.fecha DESC, av.nalbventa DESC, avl.linea ASC
         `;
 
         const { rows } = await pool.query(query, params);
